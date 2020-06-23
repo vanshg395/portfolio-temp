914794e0dd2328a87eb566132df75fb31c3fb48d
Vansh Goel <vanshgoel@Vanshs-MacBook-Pro.local>
2020-06-23 22:19:31 +0530

    unnecessary code removed

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -119,102 +119,105 @@ class _HomeViewState extends State<HomeView> {
                   ),
                 ],
               ),
-              mobile: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 50,
-                        vertical: 30,
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(color: Colors.grey, width: 2),
-                            ),
-                            child: ClipOval(
-                              child: Image.asset(
-                                'assets/img/me.jpeg',
-                                height: 150,
+              mobile: Scrollbar(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 50,
+                          vertical: 30,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border:
+                                    Border.all(color: Colors.grey, width: 2),
+                              ),
+                              child: ClipOval(
+                                child: Image.asset(
+                                  'assets/img/me.jpeg',
+                                  height: 150,
+                                ),
                               ),
                             ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            'Vansh Goel',
-                            style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
+                            SizedBox(
+                              height: 20,
                             ),
-                          ),
-                          Text(
-                            '"A Cadet Who Codes"',
-                            style: TextStyle(
-                              fontStyle: FontStyle.italic,
+                            Text(
+                              'Vansh Goel',
+                              style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Container(
-                            height: 1,
-                            width: 200,
-                            color: Colors.black,
-                          ),
-                          FlatButton(
-                            child: Text('About Me'),
-                            onPressed: () {
-                              setState(() {
-                                _selectedSectionIndex = 0;
-                              });
-                            },
-                          ),
-                          FlatButton(
-                            child: Text('Skills'),
-                            onPressed: () {
-                              setState(() {
-                                _selectedSectionIndex = 1;
-                              });
-                            },
-                          ),
-                          FlatButton(
-                            child: Text('Projects'),
-                            onPressed: () {
-                              setState(() {
-                                _selectedSectionIndex = 2;
-                              });
-                            },
-                          ),
-                          Container(
-                            height: 1,
-                            width: 200,
-                            color: Colors.black,
-                          )
-                        ],
+                            Text(
+                              '"A Cadet Who Codes"',
+                              style: TextStyle(
+                                fontStyle: FontStyle.italic,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Container(
+                              height: 1,
+                              width: 200,
+                              color: Colors.black,
+                            ),
+                            FlatButton(
+                              child: Text('About Me'),
+                              onPressed: () {
+                                setState(() {
+                                  _selectedSectionIndex = 0;
+                                });
+                              },
+                            ),
+                            FlatButton(
+                              child: Text('Skills'),
+                              onPressed: () {
+                                setState(() {
+                                  _selectedSectionIndex = 1;
+                                });
+                              },
+                            ),
+                            FlatButton(
+                              child: Text('Projects'),
+                              onPressed: () {
+                                setState(() {
+                                  _selectedSectionIndex = 2;
+                                });
+                              },
+                            ),
+                            Container(
+                              height: 1,
+                              width: 200,
+                              color: Colors.black,
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 20),
-                      width: double.infinity,
-                      height: 1,
-                      color: Colors.black,
-                    ),
-                    Container(
-                      height: 500,
-                      child: _selectedSectionIndex == 0
-                          ? AboutContent()
-                          : _selectedSectionIndex == 1
-                              ? SkillsContent()
-                              : ProjectsContent(),
-                    ),
-                  ],
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 20),
+                        width: double.infinity,
+                        height: 1,
+                        color: Colors.black,
+                      ),
+                      Container(
+                        height: 500,
+                        child: _selectedSectionIndex == 0
+                            ? AboutContent()
+                            : _selectedSectionIndex == 1
+                                ? SkillsContent()
+                                : ProjectsContent(),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
