dbe2b919d74cfe7cb2be740b8df9a3e1dc7cdec7
Vansh Goel <vanshgoel@Vanshs-MacBook-Pro.local>
2020-06-25 14:12:22 +0530

    changed shadow method

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -19,205 +19,207 @@ class _HomeViewState extends State<HomeView> {
     return Scaffold(
       backgroundColor: Color(0xFF79d70f),
       body: CenteredView(
-        child: Material(
-          color: Colors.transparent,
-          elevation: 10,
-          child: Container(
-            color: Colors.white,
-            child: ScreenTypeLayout(
-              tablet: Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 50,
-                      vertical: 30,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(color: Colors.grey, width: 2),
-                          ),
-                          child: ClipOval(
-                            child: Image.asset(
-                              'assets/img/me.jpeg',
-                              height: 150,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          'Vansh Goel',
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          '"A Cadet Who Codes"',
-                          style: TextStyle(
-                            fontStyle: FontStyle.italic,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Container(
-                          height: 1,
-                          width: 200,
-                          color: Colors.black,
-                        ),
-                        FlatButton(
-                          child: Text('About Me'),
-                          onPressed: () {
-                            setState(() {
-                              _selectedSectionIndex = 0;
-                            });
-                          },
-                        ),
-                        FlatButton(
-                          child: Text('Skills'),
-                          onPressed: () {
-                            setState(() {
-                              _selectedSectionIndex = 1;
-                            });
-                          },
-                        ),
-                        FlatButton(
-                          child: Text('Projects'),
-                          onPressed: () {
-                            setState(() {
-                              _selectedSectionIndex = 2;
-                            });
-                          },
-                        ),
-                        Container(
-                          height: 1,
-                          width: 200,
-                          color: Colors.black,
-                        )
-                      ],
-                    ),
+        child: Container(
+          decoration: BoxDecoration(color: Colors.white, boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.3),
+              offset: Offset(4, 4),
+              spreadRadius: 1,
+              blurRadius: 20,
+            )
+          ]),
+          child: ScreenTypeLayout(
+            tablet: Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 50,
+                    vertical: 30,
                   ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 20),
-                    height: double.infinity,
-                    width: 1,
-                    color: Colors.black,
-                  ),
-                  Expanded(
-                    child: _selectedSectionIndex == 0
-                        ? AboutContent()
-                        : _selectedSectionIndex == 1
-                            ? SkillsContent()
-                            : ProjectsContent(),
-                  ),
-                ],
-              ),
-              mobile: Scrollbar(
-                child: SingleChildScrollView(
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 50,
-                          vertical: 30,
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.grey, width: 2),
                         ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Container(
-                              padding: EdgeInsets.all(5),
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border:
-                                    Border.all(color: Colors.grey, width: 2),
-                              ),
-                              child: ClipOval(
-                                child: Image.asset(
-                                  'assets/img/me.jpeg',
-                                  height: 150,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Text(
-                              'Vansh Goel',
-                              style: TextStyle(
-                                fontSize: 30,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              '"A Cadet Who Codes"',
-                              style: TextStyle(
-                                fontStyle: FontStyle.italic,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Container(
-                              height: 1,
-                              width: 200,
-                              color: Colors.black,
-                            ),
-                            FlatButton(
-                              child: Text('About Me'),
-                              onPressed: () {
-                                setState(() {
-                                  _selectedSectionIndex = 0;
-                                });
-                              },
-                            ),
-                            FlatButton(
-                              child: Text('Skills'),
-                              onPressed: () {
-                                setState(() {
-                                  _selectedSectionIndex = 1;
-                                });
-                              },
-                            ),
-                            FlatButton(
-                              child: Text('Projects'),
-                              onPressed: () {
-                                setState(() {
-                                  _selectedSectionIndex = 2;
-                                });
-                              },
-                            ),
-                            Container(
-                              height: 1,
-                              width: 200,
-                              color: Colors.black,
-                            )
-                          ],
+                        child: ClipOval(
+                          child: Image.asset(
+                            'assets/img/me.jpeg',
+                            height: 150,
+                          ),
                         ),
                       ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        'Vansh Goel',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '"A Cadet Who Codes"',
+                        style: TextStyle(
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
                       Container(
-                        margin: EdgeInsets.symmetric(vertical: 20),
-                        width: double.infinity,
                         height: 1,
+                        width: 200,
                         color: Colors.black,
                       ),
-                      Container(
-                        height: 500,
-                        child: _selectedSectionIndex == 0
-                            ? AboutContent()
-                            : _selectedSectionIndex == 1
-                                ? SkillsContent()
-                                : ProjectsContent(),
+                      FlatButton(
+                        child: Text('About Me'),
+                        onPressed: () {
+                          setState(() {
+                            _selectedSectionIndex = 0;
+                          });
+                        },
                       ),
+                      FlatButton(
+                        child: Text('Skills'),
+                        onPressed: () {
+                          setState(() {
+                            _selectedSectionIndex = 1;
+                          });
+                        },
+                      ),
+                      FlatButton(
+                        child: Text('Projects'),
+                        onPressed: () {
+                          setState(() {
+                            _selectedSectionIndex = 2;
+                          });
+                        },
+                      ),
+                      Container(
+                        height: 1,
+                        width: 200,
+                        color: Colors.black,
+                      )
                     ],
                   ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 20),
+                  height: double.infinity,
+                  width: 1,
+                  color: Colors.black,
+                ),
+                Expanded(
+                  child: _selectedSectionIndex == 0
+                      ? AboutContent()
+                      : _selectedSectionIndex == 1
+                          ? SkillsContent()
+                          : ProjectsContent(),
+                ),
+              ],
+            ),
+            mobile: Scrollbar(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 50,
+                        vertical: 30,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(color: Colors.grey, width: 2),
+                            ),
+                            child: ClipOval(
+                              child: Image.asset(
+                                'assets/img/me.jpeg',
+                                height: 150,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'Vansh Goel',
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '"A Cadet Who Codes"',
+                            style: TextStyle(
+                              fontStyle: FontStyle.italic,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            height: 1,
+                            width: 200,
+                            color: Colors.black,
+                          ),
+                          FlatButton(
+                            child: Text('About Me'),
+                            onPressed: () {
+                              setState(() {
+                                _selectedSectionIndex = 0;
+                              });
+                            },
+                          ),
+                          FlatButton(
+                            child: Text('Skills'),
+                            onPressed: () {
+                              setState(() {
+                                _selectedSectionIndex = 1;
+                              });
+                            },
+                          ),
+                          FlatButton(
+                            child: Text('Projects'),
+                            onPressed: () {
+                              setState(() {
+                                _selectedSectionIndex = 2;
+                              });
+                            },
+                          ),
+                          Container(
+                            height: 1,
+                            width: 200,
+                            color: Colors.black,
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      width: double.infinity,
+                      height: 1,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      height: 500,
+                      child: _selectedSectionIndex == 0
+                          ? AboutContent()
+                          : _selectedSectionIndex == 1
+                              ? SkillsContent()
+                              : ProjectsContent(),
+                    ),
+                  ],
                 ),
               ),
             ),
